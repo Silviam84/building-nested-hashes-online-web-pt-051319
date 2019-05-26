e595601013aeee6aef6e1256e1f66444742fbc37
Silvia Mendoza <silviamendoza223@yahoo.com>
2019-05-26 00:24:20 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -7,17 +7,17 @@ def third_challenge
       :matriarch => {
         #your key/value pairs here
         },
-      :hero => {
-        #your key/value pairs here
-        },
+      :hero => {"Romeo"
+        }, #age {"15}"
+        :status "alive"
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => :name {"Lord Montague"}
+      #age {"53"}
       
-      
-      :matriarch => {
-        #your key/value pairs here
+      :matriarch => {"Lady Monague"
+        #age {"54"
         },
       :heroine => {
         #your key/value pairs here
